5ee405050371228ba13e425b781d947ee3978648
kamack38 <64226248+kamack38@users.noreply.github.com>
2024-02-08 18:58:34 +0100

    ci(CD): 🎡 Fix the file path that the changes are written to

--- scripts/fix_Cargo_lock.js
+++ scripts/fix_Cargo_lock.js
@@ -3,5 +3,5 @@ const filePath = './Cargo.lock'
 
 exports.preCommit = (props) => {
   const tomlContent = fs.readFileSync(filePath, 'utf8')
-  fs.writeFileSync('modified_dependencies.toml', tomlContent.replace(/(name = "ram-machine"[\s\S]version = )".*"/m, `$1"${props.tag}"`))
+  fs.writeFileSync(filePath, tomlContent.replace(/(name = "ram-machine"[\s\S]version = )".*"/m, `$1"${props.tag}"`))
 }
